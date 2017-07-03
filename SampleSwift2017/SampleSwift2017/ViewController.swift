2eb5955a050bc6692ff90e3be3e58060e63a914b
WindyCheng520 <WindyCheng@aliyun.com>
2017-07-03 11:55:08 +0800

    初始化项目.

---
+++ SampleSwift2017/SampleSwift2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleSwift2017
+//
+//  Created by Windy on 2017/7/3.
+//  Copyright © 2017年 Windy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
